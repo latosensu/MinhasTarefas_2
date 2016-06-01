a5eb3761983f47e85b1649662bab40aa54677f0c
Bruno <bruno.eng.comp@gmail.com>
2016-06-01 20:02:47 -0300

    Adicionar metodo filtrar

--- grails-app/controllers/br/ufscar/minhasTarefas/ListaTarefaController.groovy
+++ grails-app/controllers/br/ufscar/minhasTarefas/ListaTarefaController.groovy
@@ -10,13 +10,33 @@ class ListaTarefaController {
         ListaTarefa novaLista = new ListaTarefa(nome: nome, usuario: usuario)
 
         if (!novaLista.save()) {
-            render (novaLista.errors.allErrors as JSON)
+            render(novaLista.errors.allErrors as JSON)
             return
         }
-        render (novaLista as JSON)
+        render(novaLista as JSON)
     }
 
-    def listar(){
-        render (ListaTarefa.all as JSON)
+    def listar() {
+        render(ListaTarefa.all as JSON)
+    }
+
+    def filtrar() {
+        String nome = params.nome
+        String usuario = params.usuario
+        Boolean ativa = params.boolean('ativa')
+
+        def listasFiltradas = ListaTarefa.withCriteria() {
+            if (nome) {
+                eq('nome', nome)
+            }
+            if (usuario) {
+                eq('usuario', usuario)
+            }
+            if (ativa != null) {
+                eq('ativa', ativa)
+            }
+        }
+
+        render listasFiltradas as JSON
     }
 }
