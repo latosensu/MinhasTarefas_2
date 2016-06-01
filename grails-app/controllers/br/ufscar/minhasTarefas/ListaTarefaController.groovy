08f806b784cfa16011d40dfcf3f6b5c83b4979a3
Bruno <bruno.eng.comp@gmail.com>
2016-06-01 19:54:35 -0300

    Criar ListaTarefaController e adicionar metodo inserir

---
+++ grails-app/controllers/br/ufscar/minhasTarefas/ListaTarefaController.groovy
@@ -0,0 +1,18 @@
+package br.ufscar.minhasTarefas
+
+import grails.converters.JSON
+
+class ListaTarefaController {
+
+    def inserir() {
+        String nome = params.nome
+        String usuario = params.usuario
+        ListaTarefa novaLista = new ListaTarefa(nome: nome, usuario: usuario)
+
+        if (!novaLista.save()) {
+            render (novaLista.errors.allErrors as JSON)
+            return
+        }
+        render (novaLista as JSON)
+    }
+}
