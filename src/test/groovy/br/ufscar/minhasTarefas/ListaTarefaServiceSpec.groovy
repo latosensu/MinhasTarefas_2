3590ec58584c330ac27378a25fb668a4e4a4a2b9
Bruno <bruno.eng.comp@gmail.com>
2016-06-01 23:25:03 -0300

    Adicionar teste para listar tarefas quando existem listas cadastradas

--- src/test/groovy/br/ufscar/minhasTarefas/ListaTarefaServiceSpec.groovy
+++ src/test/groovy/br/ufscar/minhasTarefas/ListaTarefaServiceSpec.groovy
@@ -21,4 +21,20 @@ class ListaTarefaServiceSpec extends Specification {
         expect: "Lista vazia"
         service.listar().empty
     }
+
+    void "Listar listas de tarefas quando existem listas cadastradas no banco"() {
+        given: "Duas listas cadastradas"
+        def listaTarefa1 = new ListaTarefa().save(flush: true, validate: false)
+        def listaTarefa2 = new ListaTarefa().save(flush: true, validate: false)
+
+        when: "Lista as listas de tarefa"
+        def listas = service.listar()
+
+        then: "A lista tem tamanho 2"
+        listas.size() == 2
+
+        and: "A lista contém as duas listas cadastradas"
+        listas.containsAll(listaTarefa1, listaTarefa2)
+    }
+
 }
