40666ad028c74aaa10ee2648b4c1037151482f51
Bruno <bruno.eng.comp@gmail.com>
2016-06-01 23:20:17 -0300

    Adicionar annotation de Mock

--- src/test/groovy/br/ufscar/minhasTarefas/ListaTarefaServiceSpec.groovy
+++ src/test/groovy/br/ufscar/minhasTarefas/ListaTarefaServiceSpec.groovy
@@ -1,5 +1,6 @@
 package br.ufscar.minhasTarefas
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ListaTarefaService)
+@Mock(ListaTarefa)
 class ListaTarefaServiceSpec extends Specification {
 
     def setup() {
